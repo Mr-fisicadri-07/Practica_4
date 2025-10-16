875aceecd027ead234afb33f9f87c991a5a252c4
Mr-fisicadri-07 <adrianlof07@gmail.com>
2025-10-16 11:35:58 +0200

    Created area calculator

---
+++ area_calculator.py
@@ -0,0 +1,9 @@
+b = float(input("\nEnter the width of the rectangle (cm): \n"))
+
+h = float(input("\nEnter the height of the rectangle (cm): \n"))
+
+def area_of_rectangle(b, h):
+    area = b * h
+    return area
+
+print("\nThe area of the rectangle is: {:.2f} square centimeters".format (area_of_rectangle(b, h)))
